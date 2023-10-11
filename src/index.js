dd13386c511568b9226351aa66cae43d6062da26
abhishekp41 <abhishek@particle41.com>
2023-10-11 18:20:52 +0530

    final changes for production

--- src/index.js
+++ src/index.js
@@ -341,11 +341,11 @@ const setBaseUrl = (baseUrl) => {
 }
 
 const setEnvironment = (env) => {
-  // if (env == 'production') {
-  //   setBaseUrl("https://donors-accounts.prod.paymentpreemptivelove.com");
-  // } else {
+  if (env == 'production') {
+    setBaseUrl("https://donors-accounts.prod.paymentpreemptivelove.com");
+  } else {
     setBaseUrl("https://donors-accounts.stg.paymentpreemptivelove.com");
-  // }
+  }
 }
 
 module.exports = {
